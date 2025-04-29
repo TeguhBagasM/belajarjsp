84e8ced101d77928e2b403251b2539f0f08d338b
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-04-29 08:18:52 +0300

    latihan jsp

---
+++ src/java/belajarjsp/Siswa.java
@@ -0,0 +1,30 @@
+package belajarjsp;
+
+public class Siswa {
+    private String id;
+    private String nama;
+
+    public Siswa() {
+    }
+
+    public Siswa(String id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
